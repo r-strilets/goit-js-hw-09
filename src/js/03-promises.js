88ae5c0b50c6d668deace6ac0522623356ef345c
r-strilets <str.komar@gmail.com>
2022-09-07 10:17:26 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formSubmit = document.querySelector('.form');
+
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+let position = null;
+
+function createPromise({ position, delay }) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve().then((position, delay) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      });
+    } else {
+      reject().catch((position, delay) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    }
+  });
+}
+
+formSubmit.addEventListener('submit', onClickSubmit);
+
+function onClickSubmit(e) {
+  e.preventDefault();
+  console.log('123');
+
+  const { delay, step, amount } = e.target.elements;
+  console.log(delay.value);
+  for (let i = 1; i <= amount.value; i += 1) {
+    position += +step.value;
+    console.log(position);
+    createPromise(position, +delay.value);
   }
 }
